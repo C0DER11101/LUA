4bbc76ce78f6f37863b6e0cd97b43644d7629380
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-16 10:56:30 +0530

    Lua file 1(Basics)

---
+++ 1.lua
@@ -0,0 +1,19 @@
+print('hello world');
+print("Hello World");
+
+-- this is a comment
+
+--[[ 
+this is a multiline comment
+]]
+
+print("This line is after the comments");
+
+print('Hello', 'World'); -- prints Hello   World
+print("Hello", 'World'); -- prints Hello   World
+
+print("Hello"..'World'); -- prints HelloWorld
+print("Priyanuj ".."Bora"); -- prints Priyanuj Bora
+print('Priyanuj'.." ".."Bora"); -- prints Priyanuj Bora
+
+print("2 + 2 = "..4);
