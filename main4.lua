a360fd1a28b3fe1a7c72f35f79303f365277e1e1
Priyanuj Bora <while.alive.program@gmail.com>
2023-11-20 23:20:53 +0530

    Converting string to a number, calculating power of a number

---
+++ main4.lua
@@ -0,0 +1,15 @@
+-- converting a string to a number
+
+local str="20"
+
+print(type(str))
+
+str=tonumber(str)         -- converts str to a number
+
+print(type(str))
+
+-- calculating power of a number
+
+print(2^6)   -- this is not 2 XOR 6, it's 2 raised to the power of 6
+
+print(350 % 4)   -- remainder
