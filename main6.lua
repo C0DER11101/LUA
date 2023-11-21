e8f87b2eea72b98b8b6c22969cc51825e575c9a5
Priyanuj Bora <while.alive.program@gmail.com>
2023-11-21 11:14:01 +0530

    max(), min(), floor(), ceil()

---
+++ main6.lua
@@ -0,0 +1,10 @@
+-- getting minimum and maximum value from a list of numbers
+
+print("Minimum value: "..math.min(10, 1, 50, 12, 19, 22, 46))
+print("Maximum value: "..math.max(10, 1, 50, 12, 19, 22, 46))
+
+-- floor()
+print(math.floor(3.14159))   -- rounds down, will print 3
+
+-- ceil()
+print(math.ceil(3.14159))    -- rounds up, will print 4
