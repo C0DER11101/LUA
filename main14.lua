1cd0777b01d1796d5fe2f1789f32e0b95c97a052
Priyanuj Bora <while.alive.program@gmail.com>
2023-11-23 22:28:22 +0530

    Co-routines in Lua

--- main14.lua
+++ main14.lua
@@ -1,8 +1,16 @@
 -- Co-routines
-local routine1=coroutine.create(
-function ()
+--[[
+Co-routines are general control structures where flow control is co-operatively passed between two different routines without
+returning.
+]]
+
+routine1=coroutine.create(
+function()
+	print("In coroutine:")
+
 	for i=1, 10 do
-		print("Routine 1: "..i)
+		print(coroutine.status(routine1))
+		print("[Routine-1]: "..i)
 		
 		if(i==5) then
 			coroutine.yield()
@@ -11,12 +19,15 @@ function ()
 end
 )
 
-local sampleRoutine=function()
+local loop=function()
 	for i=11, 20 do
-		print("Routine 2: "..i)
+		print("[Routine-2]: "..i)
 	end
 end
 
-local routine2=coroutine.create(sampleRoutine)
+local routine2=coroutine.create(loop)
 
+coroutine.resume(routine1)   -- starts routine1
+print(coroutine.status(routine1))   -- getting the status of the coroutine
+coroutine.resume(routine1)   -- resume executing routine1
 print(coroutine.status(routine1))
