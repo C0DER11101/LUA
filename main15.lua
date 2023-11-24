a7baf0602f9b06aa5ab8d484caad3b5be14b16b2
Priyanuj Bora <while.alive.program@gmail.com>
2023-11-24 09:10:31 +0530

    File handling in LUA

--- main15.lua
+++ main15.lua
@@ -1,6 +1,6 @@
 -- Working with files
 
--- create a fil
+-- create a file
 io.output("myFile.txt")          -- this function creates a file and if the file already exists, it is going to empty the file
 
 -- writing to the file
